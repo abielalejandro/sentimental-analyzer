712ce96a2e512616b0afb22ca1b9f6bd06070fc8
main <rjgc2810@gmail.com>
2024-05-21 11:51:01 -0300

    Simulator

---
+++ web/static/main.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  let socket = new WebSocket('ws://localhost:8080/ws');
+  socket.onmessage = (event) => {
+    proccessWsData(event.data);
+  };
+
+  const form = document.getElementById('myForm');
+  form.addEventListener('submit', sendData);
+  let ul = document.getElementById('comments');
+  let sents = document.getElementById('sents');
+
+  function sendData(evt) {
+    evt.preventDefault();
+    const data = new FormData(evt.target);
+    const dataObject = Object.fromEntries(data.entries());
+    socket.send(dataObject['text']);
+    form.reset();
+  }
+
+  const processChatMessage = (msg) => {
+    let el = document.createElement('li');
+    el.classList.add('has-text-right', 'has-text-primary');
+    el.innerHTML = msg.data.Msg;
+    ul.prepend(el);
+  };
+
+  const processSentiment = (msg) => {
+    let el = document.createElement('li');
+    el.classList.add(
+      'has-text-right',
+      'is-size-1',
+      'animate__animated',
+      'animate__zoomIn',
+    );
+    el.innerHTML = msg.data;
+    sents.prepend(el);
+
+    setTimeout(() => {
+      sents.removeChild(el);
+    }, 3000);
+  };
+
+  function proccessWsData(data) {
+    const fnMap = {
+      'ws.text.created': processChatMessage,
+      'master.text.analyzed': processSentiment,
+    };
+    const msg = JSON.parse(data);
+    const fn =
+      fnMap[msg.type] ||
+      function (msg) {
+        console.log(msg);
+      };
+
+    fn(msg);
+  }
+
+  runSimulator();
+});
---
+++ web/static/simulator.js
@@ -0,0 +1,35 @@
+const runSimulator = () => {
+  let isActive = false;
+
+  if (!isActive) return;
+
+  fetch('static/seed.json')
+    .then((r) => r.json())
+    .then((data) => {
+      const positives = data.phrases.positive;
+      const negatives = data.phrases.negative;
+
+      const size =
+        positives.length > negatives.length
+          ? positives.length
+          : negatives.length;
+
+      let input = document.getElementById('text');
+      let btn = document.getElementById('send');
+      for (let i = 0; i < size; i++) {
+        setTimeout(() => {
+          if (positives[i]) {
+            input.value = positives[i];
+            btn.click();
+          }
+        }, 3000 * i);
+
+        setTimeout(() => {
+          if (negatives[i]) {
+            input.value = negatives[i];
+            btn.click();
+          }
+        }, 5000 * i);
+      }
+    });
+};
